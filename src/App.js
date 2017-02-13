4d68f7a25568295d3af1bcd0d79dd9f50e30bf94
sureshnswamy <sureshnswamy@gmail.com>
2017-02-14 12:50:32 +1300

    link data and remove react header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as d3 from 'd3'
 import './App.css';
 
 import { World } from './Maps'
 
 class App extends Component {
+
+  state = {
+    data: null
+  }
+
+  componentWillMount() {
+    d3.queue()
+      .defer(d3.csv, 'data/UN_MigrantStockByOriginAndDestination_2015_cleaned.csv',
+            (row) => {
+              console.log(row)
+              return row;
+            })
+      .await((err, data) => {
+          this.setState({
+            data:data
+          })
+      })
+  }
+
   render() {
+    console.log('here is data', this.state.data);
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        
         <p className="App-intro">
           
           <World />
